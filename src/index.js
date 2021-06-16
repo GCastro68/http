60acfb263d3c6815dc56f4a8b712bacd09d6a9d2
GCastro68 <gcastro2805@gmail.com>
2021-06-15 19:59:57 -0600

    Uso de CRUD

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 // 202106141329
 
-import { init } from './js/usuarios-page';
+// import { init } from './js/usuarios-page';
 
 // Para la consulta de Chistes
 // import { init } from './js/chistes-page';
 // init();
 
 //Para la consulta de usuarios
-init();
+// init();
+
+// ********     MANEJO DEL CRUD  202106151254
+//Esta manera de importar es para tener acceso a todos los
+//métodos de crud-provider con el alias CRUD
+import * as CRUD from './js/crud-provider';
+// CRUD.getUsuario(2).then(console.log);
+
+// CRUD.crearUsuario({
+//     name: 'Gustavo',
+//     job: 'Profr. Inglés',
+// }).then(console.log);
+
+// CRUD.actualizarUsuario(1, {
+//     name: 'Irmita',
+//     job: 'Preescolar',
+// }).then(console.log);
+
+CRUD.borrarUsuario(1).then(console.log);
